4e1b48b309af04ab070eeb3d5e6443681f36334b
Micah Wood <micah@wpscholar.com>
2017-12-13 13:33:46 -0500

    Remove template handler from Form class

--- includes/Forms/Form.php
+++ includes/Forms/Form.php
@@ -5,7 +5,6 @@ namespace FlexFields\Forms;
 use FlexFields\Fields\Field;
 use FlexFields\Fields\FieldContainer;
 use FlexFields\Make;
-use FlexFields\TemplateHandler;
 use FlexFields\Traits\Data;
 use FlexFields\Traits\Errors;
 use FlexFields\Traits\Getter;
@@ -36,7 +35,7 @@ class Form {
 	 */
 	public function __construct( $name, array $args = [] ) {
 
-		$this->_data         = $args;
+		$this->_data = $args;
 		$this->_data['name'] = $name;
 
 		// Setup handler, if provided
@@ -46,7 +45,7 @@ class Form {
 		}
 
 		// Setup attributes
-		$this->_data['atts']           = $this->getData( 'atts', [] );
+		$this->_data['atts'] = $this->getData( 'atts', [] );
 		$this->_data['atts']['action'] = $this->getData( 'action', '' );
 
 		// Set fields
@@ -60,7 +59,7 @@ class Form {
 
 		// Set core data
 		$this->context = $this->getData( 'context', 'custom' );
-		$this->method  = $this->getData( 'method', $this->getData( [ 'atts', 'method' ], 'GET' ) );
+		$this->method = $this->getData( 'method', $this->getData( [ 'atts', 'method' ], 'GET' ) );
 
 		$this->_data['action'] = $this->getData( 'action', $this->getData( [ 'atts', 'action' ], '' ) );
 
@@ -161,13 +160,13 @@ class Form {
 	 * @param string $value
 	 */
 	protected function _set_method( $value ) {
-		$value         = strtoupper( $value );
+		$value = strtoupper( $value );
 		$valid_methods = [ 'GET', 'POST' ];
-		$method        = $valid_methods[0];
+		$method = $valid_methods[0];
 		if ( in_array( $value, $valid_methods ) ) {
 			$method = $value;
 		}
-		$this->_data['method']         = $method;
+		$this->_data['method'] = $method;
 		$this->_data['atts']['method'] = $method;
 	}
 
@@ -178,14 +177,22 @@ class Form {
 	 */
 	public function __toString() {
 
-		$template = TemplateHandler::getInstance();
+		$output = '';
 
-		return $template->toString( 'form.php', [
-			'atts'      => $this->getData( 'atts', [] ),
-			'hasErrors' => $this->hasErrors(),
-			'errors'    => $this->getErrorMessages(),
-			'content'   => $this->getData( 'fields' )->__toString(),
-		] );
+		try {
+			$render = flex_fields_container()->get( 'render' );
+			$output = (string) $render( 'form.php', [
+				'atts'      => $this->getData( 'atts', [] ),
+				'hasErrors' => $this->hasErrors(),
+				'errors'    => $this->getErrorMessages(),
+				'content'   => $this->getData( 'fields' )->__toString(),
+			] );
+		} catch ( \Exception $e ) {
+			trigger_error( $e->getMessage(), E_USER_ERROR );
+		} finally {
+			return $output;
+		}
+
 	}
 
 
