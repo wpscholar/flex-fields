7b82c5faf1b335fef91227d1685b4cb2e703fef8
Micah Wood <micah@wpscholar.com>
2017-12-18 18:02:56 -0500

    Fix issue where select fields with support for multiple selections weren’t getting the correct items pre-selected.

--- templates/select-option.php
+++ templates/select-option.php
@@ -2,8 +2,12 @@
 /**
  * @var \wpscholar\TemplateX $x
  */
+
+$fieldValue = $x->get( 'value' );
+$optionValue = $x->get( 'option.value' );
+$optionLabel = $x->get( 'option.label' );
+$selected = is_array( $fieldValue ) ? in_array( $optionValue, $fieldValue, false ) : $fieldValue == $optionValue;
+
 ?>
-<option
-    value="<?php echo esc_attr( $x->get( 'option.value' ) ); ?>"
-	<?php selected( $x->is( 'value', $x->get( 'option.value' ) ) ); ?>
-><?php echo esc_html( $x->get( 'option.label' ) ); ?></option>
+<option <?php selected( $selected ); ?>
+        value="<?php echo esc_attr( $optionValue ); ?>"><?php echo esc_html( $optionLabel ); ?></option>
