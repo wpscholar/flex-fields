f175b7f0dc72a857c5e17cac970e71757e3cd3c9
Micah Wood <micah@wpscholar.com>
2018-01-22 14:13:33 -0500

    Added some helpful utility functions

--- includes/functions.php
+++ includes/functions.php
@@ -2,6 +2,7 @@
 
 use FlexFields\Container;
 use FlexFields\Fields\Field;
+use FlexFields\Forms\Form;
 use FlexFields\Make;
 
 /**
@@ -12,6 +13,19 @@ use FlexFields\Make;
  */
 function render_flex_field( $name, $group ) {
 	$field = get_flex_field( $name );
+	if ( $field && is_object( $field ) && method_exists( $field, 'render' ) ) {
+		$field->render();
+	}
+}
+
+/**
+ * Render a field from a form.
+ *
+ * @param string $name
+ * @param Form $form
+ */
+function render_flex_form_field( $name, Form $form ) {
+	$field = $form->fields->getField( $name );
 	if ( $field && is_object( $field ) && method_exists( $field, 'render' ) ) {
 		$field->render();
 	}
@@ -95,26 +109,108 @@ function get_flex_field_groups() {
 }
 
 /**
- * Fetch a field value by name from a data source (e.g. $_GET, $_POST, etc).
+ * Iterates over the current field and any child fields (if applicable).
  *
- * @param array $data
- * @param string $name
- * @param mixed $default
+ * @param Field $field
  *
- * @return mixed
+ * @return Generator
  */
-function fetch_flex_field_value_by_name( array $data, $name, $default = null ) {
-	$value = $default;
-	if ( $name && is_string( $name ) ) {
-		$path = str_replace( [ '[]', '[', ']' ], [ '|0', '|', '' ], $name );
-		$keys = explode( '|', $path );
-		foreach ( $keys as $key ) {
-			if ( ! is_array( $data ) || ! array_key_exists( $key, $data ) ) {
-				return $default;
+function flex_field_iterator( Field $field ) {
+	yield $field;
+	if ( is_a( $field, 'Traversable' ) ) {
+		/**
+		 * @var Traversable $field
+		 */
+		$fields = iterator_to_array( $field );
+		foreach ( $fields as $subField ) {
+			/**
+			 * @var Field $subField
+			 */
+			yield $subField;
+		}
+	}
+}
+
+/**
+ * Validate a field
+ *
+ * @param Field $flex_field
+ * @param Form $form
+ *
+ * @return bool
+ */
+function validate_flex_field( Field $flex_field, Form $form = null ) {
+
+	$valid = true;
+
+	foreach ( flex_field_iterator( $flex_field ) as $field ) {
+		$rules = (array) $field->getData( 'rules', [] );
+		foreach ( $rules as $rule ) {
+			if ( \is_callable( $rule ) ) {
+				$rule( $field, $form );
 			}
-			$value = $data = $data[ $key ];
+		}
+
+		if ( $field->hasErrors() ) {
+			$valid = false;
 		}
 	}
 
-	return $value;
+	return $valid;
+}
+
+/**
+ * Validate a form
+ *
+ * @param Form $form
+ *
+ * @return bool
+ */
+function validate_flex_form( Form $form ) {
+
+	$isValid = true;
+
+	foreach ( $form->fields as $field ) {
+		$valid = validate_flex_field( $field, $form );
+		if ( ! $valid ) {
+			$isValid = false;
+		}
+	}
+
+	if ( ! $isValid ) {
+		$form->addError( esc_html__( 'Sorry, there was a problem with your submission. Errors have been highlighted below.', 'flex-fields' ) );
+	}
+
+	return $isValid;
+}
+
+/**
+ * FlexFields validation rule - Used when a field is required
+ *
+ * @param Field $field
+ */
+function flex_field_value_is_required( Field $field ) {
+	$value = $field->value;
+	switch ( gettype( $value ) ) {
+		case 'string':
+			$value = trim( $value );
+			break;
+		case 'array':
+			$value = array_filter( $value );
+			break;
+	}
+	if ( empty( $value ) ) {
+		$field->addError( esc_html__( 'Field is required.', 'flex-fields' ) );
+	}
+}
+
+/**
+ * FlexFields validation rule - Used to validate an email address
+ *
+ * @param Field $field
+ */
+function flex_field_value_is_email( Field $field ) {
+	if ( ! is_email( $field->value ) ) {
+		$field->addError( esc_html__( 'Please provide a valid email address.', 'flex-fields' ) );
+	}
 }
