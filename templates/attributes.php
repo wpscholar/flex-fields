c2ef40ac56fe9a623d85f9a38e71cad49b4f95b8
Micah Wood <micah@wpscholar.com>
2018-02-01 12:49:31 -0500

    Sort attributes by name

--- templates/attributes.php
+++ templates/attributes.php
@@ -4,6 +4,9 @@
  * @var \wpscholar\TemplateX $x
  */
 
-foreach ( $x->get( 'atts', [] ) as $key => $value ) {
+$atts = (array) $x->get( 'atts', [] );
+ksort( $atts );
+
+foreach ( $atts as $key => $value ) {
 	echo ' ' . str_replace( '_', '-', sanitize_key( $key ) ) . '="' . esc_attr( $value ) . '"';
 }
